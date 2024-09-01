7fa81033618a408aa8080b2edd30efd27cbc1525
Felipe Trebino <fliphd1505@gmail.com>
2024-09-01 14:43:06 -0300

    wip: refatoracao

--- interpolacao_lagrande.py
+++ interpolacao_lagrande.py
@@ -1,21 +1,23 @@
+def lagrange(x, pontos_x, pontos_y):
 
-def lagrange(x, pontos_x, ponto_y):
+    if len(pontos_x) != len(pontos_y):
+        raise ValueError("Os vetores pontos_x e pontos_y devem ter o mesmo comprimento.")
 
     n = len(pontos_x) # Quantidade de pontos
 
     op_lagrange = [] # Vetor dos operadores de Lagrange
     
     for j in range(n):
-        l = 1 # Operador de lagrange n
+        l = 1 # Operador de lagrange para o termo j
         for i in range(n):
             if i != j:
                 #Multiplicação das diferenças
                 l *= (x - pontos_x[i])/(pontos_x[j] - pontos_x[i])  
-        Lagrange.append(l)
+        op_lagrange.append(l)
 
-    p = 0 # ponto y em x na função descrita pelos pontos
+    p = 0 # valro de y interpolado em x
     for i in range(n):
-        p += pontos_y[i] * Lagrange[i]
+        p += pontos_y[i] * op_lagrange[i]
 
     return op_lagrange, p
 
@@ -35,17 +37,17 @@ if __name__ == "__main__" :
             ponto_y = float(input(f"Insira o valor de y{i}:"))
             pontos_x.append(ponto_x)
             pontos_y.append(ponto_y)
-            if len(pontos_x) > 2 :
-                continuar = input("Quer inserir mais pontos? 's' para sim, qualquer outra coisa para não")
+            if len(pontos_x) >= 2 :
+                continuar = input("Quer inserir mais pontos? 's' para sim, qualquer outra coisa para não: ")
                 if continuar == "s":
                     continue
 
                 break
         except TypeError:
-            printf("Entrada Inválida, tente novamente")
+            print("\nEntrada Inválida, tente novamente")
 
 
-    printf("Pontos Selecionados...........................")
+    print("\nPontos Selecionados...........................")
     for i in range(len(pontos_x)):
         print(f"Ponto {i + 1} = [{pontos_x[i]}, {pontos_y[i]}]")
 
@@ -55,14 +57,14 @@ if __name__ == "__main__" :
         try:
             x = float(input("Insira o valor de x que você deseja calcular: "))
         except TypeError:
-            printf("Valor inválido")
+            print("\nValor inválido")
             x = None
 
-    operadores, p = lagrange(x, pontos_x, ponto_y)
+    operadores, p = lagrange(x, pontos_x, pontos_y)
 
     print("\nImprimindo os operadores de Lagrange...............")
 
-    for i in range(operadores):
+    for i in range(len(operadores)):
         print(f"O valor de L{i} é {operadores[i]}")
 
     print("\nImprimindo o resultado final....................")
--- interpolacao_newton.py
+++ interpolacao_newton.py
@@ -1,67 +1,128 @@
-print("\nInterpolação Polinomial - Método de Newton........")
-#n = int(input("Insira a quantidade de pontos: "))
-qtd_pontos = 3
-#while n <= 0:
-#    n = int(input("Por favor, a quantidade de pontos deve ser maior que zero.#\nInsira a quantidade de pontos: "))
-#x = float(input("Insira o valor de x que você deseja calcular: "))
-x = 1.8
+import numpy as np
 
-print("\nInsira os pontos...................................")
-pontos_x = [1, 1.5, 2]
-pontos_y = [2, 2.725, 3.414]
-d = []
-#for i in range(n):
-#    ponto_x = float(input(f"Insira o valor de x{i}:"))
-#    ponto_y = float(input(f"Insira o valor de y{i}:"))
-#    pontos_x.append(ponto_x)
-#    pontos_y.append(ponto_y)
-m = qtd_pontos - 1
-n = qtd_pontos
-pointer_y = 0
-pointer_x = 1
-for j in range(m):
-    pointer_x = j + 1
-    for i in range(n - 1):
-        num = pontos_y[pointer_y + 1] - pontos_y[pointer_y]
-        den = pontos_x[pointer_x] - pontos_x[pointer_x - (j + 1)]
-        k = []
-        pontos_y.append(num / den)
-        k.append(num)
-        k.append(den)
-        k.append(num / den)
-        d.append(k)
-        pointer_y += 1
-    pointer_y += 1
-    n -= 1
-
-print("\nImprimindo os resultados dos cálculos....................")
-print("\t  y\t        x\t Resultado")
-for linha in d:
-    print("[ ", end="")
-    for coluna in linha:
-        print(coluna, end="  ")
-    print("]")
-
-ordem = 0
-i = ordem
-print("\nImprimindo as diferenças....................")
-while ordem < qtd_pontos - 1:
-    dif = d[i][2]
-    print(f"d{ordem} = {dif}")
-    ordem += 1
-    i += qtd_pontos - ordem
-
-ordem = 1
-i = qtd_pontos - ordem
-p = pontos_y[0] + d[0][2] * (x - pontos_x[0])
-while ordem < qtd_pontos - 1:
-    aux = d[i][2]
-    for j in range(ordem + 1):
-        aux *= (x - pontos_x[j])
-    p -= aux
-    ordem += 1
-    i += qtd_pontos - ordem
-
-print("\nImprimindo o resultado final....................")
-print(f"O valor de P({x}) é {p}.")
+def newton(x, pontos_x, pontos_y):
     
+    if len(pontos_x) != len(pontos_y):
+        raise ValueError("Os vetores pontos_x e pontos_y devem ter o mesmo comprimento.")
+    
+    ordens = [] # Vetor das diferenças divididas
+    
+    n = len(pontos_x) # Quantidade de pontos
+    
+    maior_valor_da_diferenca = np.max(np.abs(pontos_y)) # Maior valor absoluto da diferença
+    
+    for i in range(n):
+        ordem = []
+        for j in range(n - i):
+            if i == 0:
+                ordem.append(pontos_y[j]) # Ordem 0
+            else:
+                temp = (ordens[i-1][j + 1] - ordens[i-1][j])/(pontos_x[j + i] - pontos_x[j])
+                
+                if(np.abs(temp) > maior_valor_da_diferenca):
+                    maior_valor_da_diferenca = temp # Obter maior valor da diferença para calcular erro posteriormente
+                
+                ordem.append(temp) # Ordem i > 0
+        
+        ordens.append(ordem)
+      
+    ponto = ordens[0][0] # Valor de y interpolado em x  
+        
+    for i in range(1,n,1):
+        dif = ordens[i][0] # Diferença na ordem i (parte de cima da diagonal)
+        dif *= np.prod([x - pontos_x[j] for j in range(i)]) # Multiplicação das diferenças em x (x - x0)(x - x1)...(x - xi-1)
+        ponto += dif
+    
+    erro = maior_valor_da_diferenca * np.prod([np.abs(x - pontos_x[j]) for j in range(n)]) # Cálculo do erro
+    
+    return ponto, erro, ordens
+    
+"""    
+
+    print("\nImprimindo os resultados dos cálculos....................")
+    print("\t  y\t        x\t Resultado")
+    for linha in d:
+        print("[ ", end="")
+        for coluna in linha:
+            print(coluna, end="  ")
+        print("]")
+
+    ordem = 0
+    i = ordem
+    print("\nImprimindo as diferenças....................")
+    while ordem < qtd_pontos - 1:
+        dif = d[i][2]
+        print(f"d{ordem} = {dif}")
+        ordem += 1
+        i += qtd_pontos - ordem
+
+    ordem = 1
+    i = qtd_pontos - ordem
+    p = pontos_y[0] + d[0][2] * (x - pontos_x[0])
+    while ordem < qtd_pontos - 1:
+        aux = d[i][2]
+        for j in range(ordem + 1):
+            aux *= (x - pontos_x[j])
+        p -= aux
+        ordem += 1
+        i += qtd_pontos - ordem
+
+    print("\nImprimindo o resultado final....................")
+    print(f"O valor de P({x}) é {p}.")
+    """
+    
+    
+if __name__ == "__main__" :
+
+    pontos_x = [0,1,2,3]
+    pontos_y = [32,47,65,92]
+    ponto, erro, ordens = newton(1.8, pontos_x, pontos_y)
+
+    print(ponto, erro)
+
+    """
+
+    print("\nInterpolação Polinomial - Método de Newton........")
+
+    pontos_x = []
+    pontos_y = []
+
+    print("\nInsira os pontos...................................")
+
+    while(True):
+        try:
+            i = len(pontos_x)
+            ponto_x = float(input(f"Insira o valor de x{i}:"))
+            ponto_y = float(input(f"Insira o valor de y{i}:"))
+            pontos_x.append(ponto_x)
+            pontos_y.append(ponto_y)
+            if len(pontos_x) >= 2 :
+                continuar = input("Quer inserir mais pontos? 's' para sim, qualquer outra coisa para não: ")
+                if continuar == "s":
+                    continue
+
+                break
+        except TypeError:
+            print("\nEntrada Inválida, tente novamente")
+
+
+    print("\nPontos Selecionados...........................")
+    for i in range(len(pontos_x)):
+        print(f"Ponto {i + 1} = [{pontos_x[i]}, {pontos_y[i]}]")
+
+    x = None
+
+    while(x == None):
+        try:
+            x = float(input("Insira o valor de x que você deseja calcular: "))
+        except TypeError:
+            print("\nValor inválido")
+            x = None
+
+    
+        
+
+    print("\nImprimindo o resultado final....................")
+    print(f"O valor de P({x}) é {p}.")
+    
+    """
